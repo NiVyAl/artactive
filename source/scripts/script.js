a80d0d36827d6f1a53a5ca7c91652a9455d79696
nick <nikitavisoc@gmail.com>
2018-12-09 22:39:32 +0300

    резина, меню открывается

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -2,15 +2,22 @@
 
 var menuButton = document.querySelector(".burger-button");
 var isMenuOpen = false;
+var header = document.querySelector(".page-header");
+var siteMenu = header.querySelector(".site-menu");
+console.log(header);
 menuButton.classList.add('burger-button--open');
 menuButton.addEventListener('click', function() {
   if (isMenuOpen) {
     menuButton.classList.add('burger-button--open');
     menuButton.classList.remove('burger-button--close');
+    header.classList.remove('page-header--menu-open');
+    siteMenu.classList.remove('site-menu--open');
     isMenuOpen = false;
   } else {
     menuButton.classList.add('burger-button--close');
     menuButton.classList.remove('burger-button--open');
+    header.classList.add('page-header--menu-open');
+    siteMenu.classList.add('site-menu--open');
     isMenuOpen = true;
   }
 })
